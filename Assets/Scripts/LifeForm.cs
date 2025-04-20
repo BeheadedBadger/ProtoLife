581fec3f75cf1827be8780f3fd5dad0688d25a9a
BeheadedBadger <ZarradeRuijter@gmail.com>
2025-04-20 20:18:47 +0200

    Minor improvements and additions

--- Assets/Scripts/LifeForm.cs
+++ Assets/Scripts/LifeForm.cs
@@ -187,9 +187,18 @@ public class LifeForm : MonoBehaviour
             previousUpdate = gameManager.currentDate;
         }
 
-        if (dead && health == 0 || dead && age >= (lifeFormObject.lifeSpan * 1.1))
+        if (dead)
         {
-            StartCoroutine(CoverOrStationaryDestroyAnimation(this.gameObject, new Vector3(0, 0, 0), new Vector3(1, 1, 1), 0.3f));
+            if (gameManager.currentDate.Hour > previousUpdate.Hour)
+            {
+                age++;
+            }
+
+            previousUpdate = gameManager.currentDate;
+            if (health == 0 || age >= (lifeFormObject.lifeSpan * 1.1))
+            {
+                StartCoroutine(CoverOrStationaryDestroyAnimation(this.gameObject, new Vector3(0, 0, 0), new Vector3(1, 1, 1), 0.3f));
+            }
         }
     }
 
